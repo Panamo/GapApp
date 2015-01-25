d3aa53fd0ce9d894ac3217cb6358c2e6d9aa5b02
Mohammad Mahboubi <mrmahboubi95@yahoo.com>
2015-01-25 12:41:48 +0330

    fixing some conflicts.

--- Client/src/client/RecieveThread.java
+++ Client/src/client/RecieveThread.java
@@ -23,40 +23,13 @@ public class RecieveThread extends Thread {
 		}
 		String text;
 		while (true) {
-<<<<<<< HEAD
 			// get the received stream
 			// who is sender?
 			// if sender's frame is open -> show in frame
 			// else save in a txt file for later use
 
-=======
-			try {
-				text = in.readLine();
-//				if (text.equals("onlines:")){
-//					user.getFrame().getListData().clear();
-//					int onlines = Integer.parseInt(in.readLine());
-//					for(int i = 0;i<onlines;i++){
-//						user.getFrame().getListData().add(in.readLine());
-//					}
-//					user.getFrame().getContentPane().revalidate();
-//					user.getFrame().getContentPane().repaint();
-//				}
-				//else{
-					user.getFrame()
-					.getTextPane()
-					.setText(
-							user.getFrame().getTextPane().getText()
-									+ "Server said: " + text + "\n");
-					String history;
-					if ((history = user.getFrame().getTextPane().getText()).split("\n").length>=7){
-						user.getFrame().getTextPane().setText("");
-					}
-				//}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
->>>>>>> da6058e423023250c4405b7628900516bb3aad51
 		}
+
 	}
+
 }
