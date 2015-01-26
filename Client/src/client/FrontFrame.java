d86dd7b8fddf6b2ad521236123f162b8135dbf65
navidmsk <navidmsk@yahoo.com>
2015-01-26 13:55:45 +0330

    -

--- Client/src/client/FrontFrame.java
+++ Client/src/client/FrontFrame.java
@@ -1,25 +1,21 @@
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JList;
+import java.awt.event.ActionListener;
 import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 public class FrontFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-
 
 	public FrontFrame(Client user) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,12 +24,12 @@ public class FrontFrame extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		textField = new JTextField();
 		textField.setBounds(10, 11, 313, 36);
 		contentPane.add(textField);
 		textField.setColumns(10);
-		
+
 		JButton btnSrch = new JButton("search");
 		btnSrch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -41,25 +37,25 @@ public class FrontFrame extends JFrame {
 		});
 		btnSrch.setBounds(335, 11, 89, 36);
 		contentPane.add(btnSrch);
-		
+
 		Vector<String> listV = new Vector<String>();
 		listV.add("Parham");
 		listV.add("Navid");
-		
+
 		JButton btnSelect = new JButton("Select");
 		btnSelect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//Open friend's ChatFrame:
-				//open=true;
+				// Open friend's ChatFrame:
+				// open=true;
 			}
 		});
 		btnSelect.setBounds(335, 58, 89, 192);
 		contentPane.add(btnSelect);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 58, 313, 192);
 		contentPane.add(scrollPane);
-		
+
 		JList list = new JList(listV);
 		scrollPane.setViewportView(list);
 	}
